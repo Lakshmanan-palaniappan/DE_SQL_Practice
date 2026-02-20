ef472de1f176e61949826d732db7569a59d95b58
Lakshmanan Palaniappan <154072727+Lakshmanan-palaniappan@users.noreply.github.com>
2026-02-20 18:58:59 +0530

    feat: Practiced Insert

---
+++ src/dml/insert.sql
@@ -0,0 +1,23 @@
+USE ShowBook;
+GO
+
+INSERT INTO Movies (Title, DurationMinutes)
+VALUES ('Leo', 148);
+
+INSERT INTO Movies (Title, DurationMinutes)
+VALUES 
+('GOAT', 169),
+('JN', 150);
+
+
+INSERT INTO Customers (FullName, Email)
+VALUES 
+('Cus 1', 'cus1@test.com'),
+('Cus 2', 'cus2@test.com');
+
+
+
+INSERT INTO Shows (MovieID, ShowTime, Price)
+SELECT MovieID, '2026-03-01 18:00:00', 250.00
+FROM Movies
+WHERE Title = 'Leo';
